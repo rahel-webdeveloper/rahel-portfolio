266c071b725182fcd5d9a2183d3b968bc3f50ba4
Khatibullah Rahel <khatibullahrahel25@gmail.com>
2025-05-26 17:11:20 +0430

    Create infinte moving cards

---
+++ components/Clients.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { InfiniteMovingCards } from "./ui/Infinit-moving-cards";
+import { companies, testimonials } from "@/data";
+
+const Clients = () => {
+  return (
+    <div className="py-20" id="testimonials">
+      <h1 className="heading">
+        Kind words from
+        <span className="text-purple"> satisfied clients</span>
+      </h1>
+
+      <div className="flex flex-col items-center max-lg:mt-10 mt-12">
+        <InfiniteMovingCards items={testimonials} speed="slow" />
+
+        <div className="flex items-center justify-center flex-wrap gap-4 md:gap-16 mt-8 max-lg:mt-10">
+          {companies.map(({ id, name, img, nameImg }) => (
+            <div key={id} className="flex md:max-w-60 max-w-32 gap-2">
+              <img src={img} alt={name} className="md:w-10 w-5" />
+
+              <img src={nameImg} alt={name} className="md:w-24 w-20" />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Clients;
--- components/Grid.tsx
+++ components/Grid.tsx
@@ -3,7 +3,7 @@ import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
 
 const Grid = () => {
   return (
-    <section>
+    <section id="about">
       <BentoGrid>
         {gridItems.map(
           ({
